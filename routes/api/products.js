b810bbfb1acfd58ec80eebfbd4462a23e9bcc311
Andrii Kharchenko <kh.andrew16@gmail.com>
2024-01-21 21:13:32 +0200

    fix validation products

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const ctrl = require("../../controllers/products");
-const { authenticate, validateBody } = require("../../midlewares");
-const { schemas } = require("../../models/product");
+const { authenticate } = require("../../midlewares");
+// const { schemas } = require("../../models/product");
 
 const router = express.Router();
 
 router.get("/", authenticate, ctrl.getAllProducts);
-router.get("/blood", authenticate, validateBody(schemas.filterSchema), ctrl.getProductsByBlood)
+router.get("/blood", authenticate, ctrl.getProductsByBlood);
 
 module.exports = router;
